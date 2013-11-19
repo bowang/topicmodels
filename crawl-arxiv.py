eca0926d068400de2faeee2eb8f9c6ce9fb9cc84
Bo Wang <david.b.wang@gmail.com>
2013-11-18 17:48:12 -0800

    Skip existing files when crawling arXiv

--- crawl-arxiv.py
+++ crawl-arxiv.py
@@ -4,11 +4,17 @@ import numpy as np
 from bs4 import BeautifulSoup
 import requests
 import time
+import os
 
 for year in range(1992, 2014):
 	for month in range(1, 13):
 		id = str(year * 100 + month)[2:]
-		print id
+		print id,
+		if os.path.isfile(id + '.txt'):
+			print 'already exists, skip'
+			continue
+		else:
+			print 'fetch'
 		site = requests.get('http://arxiv.org/list/cond-mat/' + id + '?show=3000')
 		soup = BeautifulSoup(site.text.encode('ISO-8859-1'))
 		f = open(id + '.txt', 'w')
