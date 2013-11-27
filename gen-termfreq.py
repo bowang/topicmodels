023ac665f4918e1c26b2d63552adad8544abeea5
Bo Wang <david.b.wang@gmail.com>
2013-11-27 01:35:33 -0800

    Add nobes to switch on/off term/phrase generation

--- gen-termfreq.py
+++ gen-termfreq.py
@@ -11,8 +11,10 @@ min_term_freq = 5
 min_phrase_freq = 3
 min_term_length = 3
 max_phrase_length = 5
-print_limit = 100000
 print_top = 30
+
+enable_term = True
+enable_phrase = True
 
 p = inflect.engine()
 stopwords = set()
@@ -67,8 +69,6 @@ def main():
       if line in ['\n', '\r\n']:
         docid += 1
         docs[docid] = {}
-        if docid % 100 == 0:
-          print 'docid = ' + str(docid)
         continue
       segments = re.split('[,;.:?!()]+', line)
       # skip doc hash tag
@@ -81,6 +81,7 @@ def main():
         for token in segment.split():
           # remove leading and trailing non alphanumeric characters
           token = token.replace('--', '-').lstrip('\"\'`~!-+*_').rstrip('\"\'`!~-+*_')
+
           # remove latex format tokens
           # remove stop words
           # remove common verbs
@@ -99,7 +100,9 @@ def main():
               singular = p.singular_noun(token)
               if singular != False:
                 token = singular.lower()
-            # generate word groups
+
+          # generate word groups
+          if enable_phrase:
             if seqlen < max_phrase_length - 1:
               prevs[seqlen] = token
               seqlen += 1
@@ -110,35 +113,43 @@ def main():
             for i in range(seqlen - 2, -1, -1):
               phrase = prevs[i] + ' ' + phrase
               addPhrase(phrase, docid)
+
           # generate words
-          token = stem(token)
-          addTerm (token, docid)
+          if enable_term:
+            token = stem(token)
+            addTerm (token, docid)
 
-  vocab_eff = OrderedDict(sorted(filter(lambda (k, v): v >= min_term_freq, vocab.items())))
-  phrases_eff = OrderedDict(sorted(filter(lambda (k, v): v >= min_phrase_freq, phrases.items())))
-  vocab_eff_keys = vocab_eff.keys()
-  phrases_eff_keys = phrases_eff.keys()
-  num_vocab_eff = len(vocab_eff)
-  num_phrases_eff = len(phrases_eff)
+  num_vocab_eff = 0
+  num_phrases_eff = 0
 
-  print 'effective vocabulary ({}): '.format(num_vocab_eff)
-  if num_vocab_eff < print_limit:
+  if enable_term:
+    vocab_eff = OrderedDict(sorted(filter(lambda (k, v): v >= min_term_freq, vocab.items())))
+    vocab_eff_keys = vocab_eff.keys()
+    num_vocab_eff = len(vocab_eff)
+    print 'effective vocabulary ({}): '.format(num_vocab_eff)
     print vocab_eff
     print ''
-  print 'effective phrases ({}): '.format(num_phrases_eff)
-  if num_phrases_eff < print_limit:
+
+  if enable_phrase:
+    phrases_eff = OrderedDict(sorted(filter(lambda (k, v): v >= min_phrase_freq, phrases.items())))
+    phrases_eff_keys = phrases_eff.keys()
+    num_phrases_eff = len(phrases_eff)
+    print 'effective phrases ({}): '.format(num_phrases_eff)
     print phrases_eff
     print ''
 
   # print top terms/phrases
-  vocab_eff_rvs = sorted(vocab_eff.iteritems(), key=operator.itemgetter(1), reverse=True)
-  phrases_eff_rvs = sorted(phrases_eff.iteritems(), key=operator.itemgetter(1), reverse=True)
-  print 'top {} out of {} terms'.format(print_top, num_vocab_eff)
-  for i in range(1, print_top + 1):
-    print vocab_eff_rvs[i]
-  print '\ntop {} out of {} phrases'.format(print_top, num_phrases_eff)
-  for i in range(1, print_top + 1):
-    print phrases_eff_rvs[i]
+  if enable_term:
+    vocab_eff_rvs = sorted(vocab_eff.iteritems(), key=operator.itemgetter(1), reverse=True)
+    print 'top {} out of {} terms'.format(print_top, num_vocab_eff)
+    for i in range(1, print_top + 1):
+      print vocab_eff_rvs[i]
+
+  if enable_phrase:
+    phrases_eff_rvs = sorted(phrases_eff.iteritems(), key=operator.itemgetter(1), reverse=True)
+    print '\ntop {} out of {} phrases'.format(print_top, num_phrases_eff)
+    for i in range(1, print_top + 1):
+      print phrases_eff_rvs[i]
 
   # generate term:freq matrix
   tffile = open('termfreq.txt', 'w')
@@ -146,21 +157,25 @@ def main():
     tflist = ''
     tfnum = 0
     for term in docs[docid]:
-      if term in vocab_eff:
-        tflist += ' ' + str(vocab_eff_keys.index(term)) + ':' + str(docs[docid][term])
-        tfnum += 1
-      elif term in phrases_eff:
-        tflist += ' ' + str(phrases_eff_keys.index(term) + num_vocab_eff) + ':' + str(docs[docid][term])
-        tfnum += 1
+      if enable_term:
+        if term in vocab_eff:
+          tflist += ' ' + str(vocab_eff_keys.index(term)) + ':' + str(docs[docid][term])
+          tfnum += 1
+      if enable_phrase:
+        if term in phrases_eff:
+          tflist += ' ' + str(phrases_eff_keys.index(term) + num_vocab_eff) + ':' + str(docs[docid][term])
+          tfnum += 1
     tffile.write(str(tfnum) + tflist + '\n')
   tffile.close()
 
   # generate dictionary
   dicfile = open('dictionary.txt', 'w')
-  for term in vocab_eff:
-    dicfile.write('\"' + term + '\"\n')
-  for term in phrases_eff:
-    dicfile.write('\"' + term + '\"\n')
+  if enable_term:
+    for term in vocab_eff:
+      dicfile.write('\"' + term + '\"\n')
+  if enable_phrase:
+    for term in phrases_eff:
+      dicfile.write('\"' + term + '\"\n')
   dicfile.close()
 
   # generate docmap
@@ -168,4 +183,5 @@ def main():
   for docid in range(1, len(docs) + (len(docs[len(docs)]) > 0)):
     dmfile.write('doc' + str(docid) + '\n');
   dmfile.close()
+
 if  __name__ =='__main__':main()
